d2976ba0ef759f7549c8d3a5310443fda997b422
Josh Campbell <joshdcam@gmail.com>
2024-08-11 18:06:17 +1000

    Bug testing

--- winforge.ps1
+++ winforge.ps1
@@ -730,29 +730,28 @@ function Add-RegistryEntries {
             Write-SystemMessage -title "Adding Registry Entries"
             foreach ($key in $registrySection.Keys) {
                 $entry = $registrySection[$key] -split ","
-                if ($entry.Length -eq 4) {
-                    $path = ($entry[0] -split "=")[1].Trim().Trim('"')
-                    $name = ($entry[1] -split "=")[1].Trim().Trim('"')
-                    $type = ($entry[2] -split "=")[1].Trim().Trim('"')
-                    $value = ($entry[3] -split "=")[1].Trim().Trim('"')
-                    
-                    # Log raw data
-                    Write-Log "Raw entry: Path=$($entry[0]), Name=$($entry[1]), Type=$($entry[2]), Value=$($entry[3])"
-                    Write-Log "Parsed data: Path=$path, Name=$name, Type=$type, Value=$value"
-                    
-                    # Expand environment variables in the value
-                    $expandedValue = [System.Environment]::ExpandEnvironmentVariables($value)
-                    
-                    # Log the registry operation
-                    Write-Log "Adding registry entry: Path=${path}, Name=${name}, Type=${type}, Value=${expandedValue}"
-                    Write-SystemMessage -msg1 "- Adding: " -msg2 "Path=${path}, Name=${name}, Type=${type}, Value=${expandedValue}"
+                
+                # Log raw data
+                Write-Log "Raw entry: $($entry[0]), $($entry[1]), $($entry[2]), $($entry[3])"
+                
+                # Parsing data
+                $path = ($entry[0] -split "=")[1].Trim().Trim('"')
+                $name = ($entry[1] -split "=")[1].Trim().Trim('"')
+                $type = ($entry[2] -split "=")[1].Trim().Trim('"')
+                $value = ($entry[3] -split "=")[1].Trim().Trim('"')
+                
+                # Log parsed data
+                Write-Log "Parsed data: Path=$path, Name=$name, Type=$type, Value=$value"
+                
+                # Expand environment variables in the value
+                $expandedValue = [System.Environment]::ExpandEnvironmentVariables($value)
+                
+                # Log the registry operation
+                Write-Log "Adding registry entry: Path=${path}, Name=${name}, Type=${type}, Value=${expandedValue}"
+                Write-SystemMessage -msg1 "- Adding: " -msg2 "Path=${path}, Name=${name}, Type=${type}, Value=${expandedValue}"
 
-                    # Use RegistryTouch for adding the registry entry
-                    RegistryTouch -action "add" -path $path -name $name -type $type -value $expandedValue
-                } else {
-                    Write-Log "Invalid registry entry format: $key"
-                    Write-ErrorMessage -msg "Invalid registry entry format: $key"
-                }
+                # Use RegistryTouch for adding the registry entry
+                RegistryTouch -action "add" -path $path -name $name -type $type -value $expandedValue
             }
             Write-Log "Registry entries added successfully."
             Write-SuccessMessage -msg "Registry entries added successfully."
@@ -765,6 +764,7 @@ function Add-RegistryEntries {
         Write-ErrorMessage -msg "Error adding registry entries: $($_.Exception.Message)"
     }
 }
+
 
 
 
